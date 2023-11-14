52767a358737e1715b95f2ef30ce959c3c3b74f8
Erickjxe <erik.santiago.sepulveda.cortes@gmail.com>
2023-11-14 08:12:19 -0500

    progresito

--- Backend/index.js
+++ Backend/index.js
@@ -1,13 +1,25 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const usuarios = require('./routes/usuarios');
+const routes = require('./routes/usuarios');
+const cursos = require('./routes/cursos');
+const cursosusuarios = require('./routes/cursosusuarios');
 
+//importante para conexion del front con el back
+//Uso de cors
 app.use(cors({origin: '*'}));
+// conectar con el link de su frontend app.use(cors({origin: 'https://ppi-app.vercel.app/'}))
+app.set('port' ,process.env.PORT || 4001);
 
-app.set('port',process.env.PORT||4001);
-app.use('/api/registro',routes)
+app.use(express.json());
 
-app-listen(app.get('port'),()=>{
-   console.log(`Server on port ${app.get('port')}`); 
+
+//rutas
+app.use('/api/usuarios',routes);
+app.use('/api/cursos',cursos);
+app.use('/api/cursosusuarios',cursosusuarios);
+
+
+app.listen(app.get('port'), () => {
+    console.log(`Server on port ${app.get('port')}`);
 });
